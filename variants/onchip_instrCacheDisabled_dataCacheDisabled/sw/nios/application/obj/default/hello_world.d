fdc88d2df983df64cac7679eae9e86c3a32e3735
Wilhelm Widlund Mellergård <wilwi905@student.liu.se>
2023-03-15 13:45:52 +0100

    Baseline

---
+++ variants/onchip_instrCacheDisabled_dataCacheDisabled/sw/nios/application/obj/default/hello_world.d
@@ -0,0 +1,16 @@
+obj/default/hello_world.o: hello_world.c \
+ C:/RTES/variants/onchip_instrCacheDisabled_dataCacheDisabled/sw/nios/onchip_instrCacheDisabled_dataCacheDisabled_bsp/system.h \
+ C:/RTES/variants/onchip_instrCacheDisabled_dataCacheDisabled/sw/nios/onchip_instrCacheDisabled_dataCacheDisabled_bsp/linker.h \
+ c:\rtes\variants\onchip_instrcachedisabled_datacachedisabled\sw\nios\onchip_instrcachedisabled_datacachedisabled_bsp\hal\inc\io.h \
+ c:\rtes\variants\onchip_instrcachedisabled_datacachedisabled\sw\nios\onchip_instrcachedisabled_datacachedisabled_bsp\hal\inc\alt_types.h \
+ c:\rtes\variants\onchip_instrcachedisabled_datacachedisabled\sw\nios\onchip_instrcachedisabled_datacachedisabled_bsp\drivers\inc\altera_avalon_pio_regs.h
+
+C:/RTES/variants/onchip_instrCacheDisabled_dataCacheDisabled/sw/nios/onchip_instrCacheDisabled_dataCacheDisabled_bsp/system.h:
+
+C:/RTES/variants/onchip_instrCacheDisabled_dataCacheDisabled/sw/nios/onchip_instrCacheDisabled_dataCacheDisabled_bsp/linker.h:
+
+c:\rtes\variants\onchip_instrcachedisabled_datacachedisabled\sw\nios\onchip_instrcachedisabled_datacachedisabled_bsp\hal\inc\io.h:
+
+c:\rtes\variants\onchip_instrcachedisabled_datacachedisabled\sw\nios\onchip_instrcachedisabled_datacachedisabled_bsp\hal\inc\alt_types.h:
+
+c:\rtes\variants\onchip_instrcachedisabled_datacachedisabled\sw\nios\onchip_instrcachedisabled_datacachedisabled_bsp\drivers\inc\altera_avalon_pio_regs.h:
