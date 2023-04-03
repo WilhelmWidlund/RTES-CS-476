2176381e8b35eae9c378273d5c9fdc33fcf74e90
Wilhelm Widlund Mellergård <wilwi905@student.liu.se>
2023-04-03 13:50:42 +0200

    ALL the code finished!

---
+++ variants/uCOS_version/hw/quartus/soc_system/soc_system_inst.vhd
@@ -0,0 +1,37 @@
+	component soc_system is
+		port (
+			clk_clk                           : in    std_logic                     := 'X';             -- clk
+			new_sdram_controller_0_wire_addr  : out   std_logic_vector(12 downto 0);                    -- addr
+			new_sdram_controller_0_wire_ba    : out   std_logic_vector(1 downto 0);                     -- ba
+			new_sdram_controller_0_wire_cas_n : out   std_logic;                                        -- cas_n
+			new_sdram_controller_0_wire_cke   : out   std_logic;                                        -- cke
+			new_sdram_controller_0_wire_cs_n  : out   std_logic;                                        -- cs_n
+			new_sdram_controller_0_wire_dq    : inout std_logic_vector(15 downto 0) := (others => 'X'); -- dq
+			new_sdram_controller_0_wire_dqm   : out   std_logic_vector(1 downto 0);                     -- dqm
+			new_sdram_controller_0_wire_ras_n : out   std_logic;                                        -- ras_n
+			new_sdram_controller_0_wire_we_n  : out   std_logic;                                        -- we_n
+			pin_buttons_export                : in    std_logic_vector(3 downto 0)  := (others => 'X'); -- export
+			pll_0_sdram_clk                   : out   std_logic;                                        -- clk
+			pout_custom_readdata              : out   std_logic_vector(31 downto 0);                    -- readdata
+			reset_reset_n                     : in    std_logic                     := 'X'              -- reset_n
+		);
+	end component soc_system;
+
+	u0 : component soc_system
+		port map (
+			clk_clk                           => CONNECTED_TO_clk_clk,                           --                         clk.clk
+			new_sdram_controller_0_wire_addr  => CONNECTED_TO_new_sdram_controller_0_wire_addr,  -- new_sdram_controller_0_wire.addr
+			new_sdram_controller_0_wire_ba    => CONNECTED_TO_new_sdram_controller_0_wire_ba,    --                            .ba
+			new_sdram_controller_0_wire_cas_n => CONNECTED_TO_new_sdram_controller_0_wire_cas_n, --                            .cas_n
+			new_sdram_controller_0_wire_cke   => CONNECTED_TO_new_sdram_controller_0_wire_cke,   --                            .cke
+			new_sdram_controller_0_wire_cs_n  => CONNECTED_TO_new_sdram_controller_0_wire_cs_n,  --                            .cs_n
+			new_sdram_controller_0_wire_dq    => CONNECTED_TO_new_sdram_controller_0_wire_dq,    --                            .dq
+			new_sdram_controller_0_wire_dqm   => CONNECTED_TO_new_sdram_controller_0_wire_dqm,   --                            .dqm
+			new_sdram_controller_0_wire_ras_n => CONNECTED_TO_new_sdram_controller_0_wire_ras_n, --                            .ras_n
+			new_sdram_controller_0_wire_we_n  => CONNECTED_TO_new_sdram_controller_0_wire_we_n,  --                            .we_n
+			pin_buttons_export                => CONNECTED_TO_pin_buttons_export,                --                 pin_buttons.export
+			pll_0_sdram_clk                   => CONNECTED_TO_pll_0_sdram_clk,                   --                 pll_0_sdram.clk
+			pout_custom_readdata              => CONNECTED_TO_pout_custom_readdata,              --                 pout_custom.readdata
+			reset_reset_n                     => CONNECTED_TO_reset_reset_n                      --                       reset.reset_n
+		);
+
---
+++ variants/uCOS_version/sw/nios/application/obj/default/hello_ucosii.d
@@ -0,0 +1,55 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\includes.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\os_cpu.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\sys/alt_irq.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\nios2.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\alt_types.h \
+ C:/RTES/variants/uCOS_version/sw/nios/uCOS_version_bsp/system.h \
+ C:/RTES/variants/uCOS_version/sw/nios/uCOS_version_bsp/linker.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\ucosii\inc\os_cfg.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\sys\alt_alarm.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\sys\alt_llist.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\priv\alt_alarm.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\ucosii\inc\ucos_ii.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\os_cpu.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\io.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\alt_types.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\sys\alt_irq.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\drivers\inc\altera_avalon_timer_regs.h \
+ c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\drivers\inc\altera_avalon_pio_regs.h
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\includes.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\os_cpu.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\sys/alt_irq.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\nios2.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\alt_types.h:
+
+C:/RTES/variants/uCOS_version/sw/nios/uCOS_version_bsp/system.h:
+
+C:/RTES/variants/uCOS_version/sw/nios/uCOS_version_bsp/linker.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\ucosii\inc\os_cfg.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\sys\alt_alarm.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\sys\alt_llist.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\priv\alt_alarm.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\ucosii\inc\ucos_ii.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\os_cpu.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\io.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\alt_types.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\hal\inc\sys\alt_irq.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\drivers\inc\altera_avalon_timer_regs.h:
+
+c:\rtes\variants\ucos_version\sw\nios\ucos_version_bsp\drivers\inc\altera_avalon_pio_regs.h:
