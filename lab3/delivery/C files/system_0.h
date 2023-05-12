3362062141fe35fad50c4c7ce3dfe73128c624cb
Wilhelm Widlund Mellergård <wilwi905@student.liu.se>
2023-05-12 14:28:33 +0200

    Final report and delivery stuff for Lab3

--- lab3/delivery/C files/system_0.h
+++ lab3/delivery/C files/system_0.h
@@ -1,10 +1,10 @@
 /*
  * system.h - SOPC Builder system and BSP software package information
  *
- * Machine generated for CPU 'cpu_1' in SOPC Builder design 'soc_system'
+ * Machine generated for CPU 'cpu_0' in SOPC Builder design 'soc_system'
  * SOPC Builder design path: C:/RTES/lab3/hw/quartus/soc_system.sopcinfo
  *
- * Generated: Mon May 01 12:44:58 CEST 2023
+ * Generated: Fri May 05 16:24:26 CEST 2023
  */
 
 /*
@@ -91,7 +91,7 @@
 #define ALT_CPU_ICACHE_SIZE 4096
 #define ALT_CPU_INITDA_SUPPORTED
 #define ALT_CPU_INST_ADDR_WIDTH 0x1b
-#define ALT_CPU_NAME "cpu_1"
+#define ALT_CPU_NAME "cpu_0"
 #define ALT_CPU_NUM_OF_SHADOW_REG_SETS 0
 #define ALT_CPU_OCI_VERSION 1
 #define ALT_CPU_RESET_ADDR 0x04020000
@@ -137,20 +137,6 @@
 
 
 /*
- * Debug_PP configuration
- *
- */
-
-#define ALT_MODULE_CLASS_Debug_PP altera_up_avalon_parallel_port
-#define DEBUG_PP_BASE 0x4041090
-#define DEBUG_PP_IRQ -1
-#define DEBUG_PP_IRQ_INTERRUPT_CONTROLLER_ID -1
-#define DEBUG_PP_NAME "/dev/Debug_PP"
-#define DEBUG_PP_SPAN 16
-#define DEBUG_PP_TYPE "altera_up_avalon_parallel_port"
-
-
-/*
  * Define for each module class mastered by the CPU
  *
  */
@@ -161,6 +147,7 @@
 #define __ALTERA_AVALON_NEW_SDRAM_CONTROLLER
 #define __ALTERA_AVALON_ONCHIP_MEMORY2
 #define __ALTERA_AVALON_PERFORMANCE_COUNTER
+#define __ALTERA_AVALON_PIO
 #define __ALTERA_AVALON_TIMER
 #define __ALTERA_NIOS2_GEN2
 #define __ALTERA_UP_AVALON_PARALLEL_PORT
@@ -182,21 +169,21 @@
 #define ALT_NUM_EXTERNAL_INTERRUPT_CONTROLLERS 0
 #define ALT_NUM_INTERNAL_INTERRUPT_CONTROLLERS 1
 #define ALT_NUM_INTERRUPT_CONTROLLERS 1
-#define ALT_STDERR "/dev/jtag_uart_1"
-#define ALT_STDERR_BASE 0x40410d0
-#define ALT_STDERR_DEV jtag_uart_1
+#define ALT_STDERR "/dev/jtag_uart_0"
+#define ALT_STDERR_BASE 0x40410e8
+#define ALT_STDERR_DEV jtag_uart_0
 #define ALT_STDERR_IS_JTAG_UART
 #define ALT_STDERR_PRESENT
 #define ALT_STDERR_TYPE "altera_avalon_jtag_uart"
-#define ALT_STDIN "/dev/jtag_uart_1"
-#define ALT_STDIN_BASE 0x40410d0
-#define ALT_STDIN_DEV jtag_uart_1
+#define ALT_STDIN "/dev/jtag_uart_0"
+#define ALT_STDIN_BASE 0x40410e8
+#define ALT_STDIN_DEV jtag_uart_0
 #define ALT_STDIN_IS_JTAG_UART
 #define ALT_STDIN_PRESENT
 #define ALT_STDIN_TYPE "altera_avalon_jtag_uart"
-#define ALT_STDOUT "/dev/jtag_uart_1"
-#define ALT_STDOUT_BASE 0x40410d0
-#define ALT_STDOUT_DEV jtag_uart_1
+#define ALT_STDOUT "/dev/jtag_uart_0"
+#define ALT_STDOUT_BASE 0x40410e8
+#define ALT_STDOUT_DEV jtag_uart_0
 #define ALT_STDOUT_IS_JTAG_UART
 #define ALT_STDOUT_PRESENT
 #define ALT_STDOUT_TYPE "altera_avalon_jtag_uart"
@@ -204,17 +191,31 @@
 
 
 /*
- * custom_counter_1 configuration
+ * custom_counter_0 configuration
  *
  */
 
-#define ALT_MODULE_CLASS_custom_counter_1 custom_counter_0
-#define CUSTOM_COUNTER_1_BASE 0x4041060
-#define CUSTOM_COUNTER_1_IRQ -1
-#define CUSTOM_COUNTER_1_IRQ_INTERRUPT_CONTROLLER_ID -1
-#define CUSTOM_COUNTER_1_NAME "/dev/custom_counter_1"
-#define CUSTOM_COUNTER_1_SPAN 32
-#define CUSTOM_COUNTER_1_TYPE "custom_counter_0"
+#define ALT_MODULE_CLASS_custom_counter_0 custom_counter_0
+#define CUSTOM_COUNTER_0_BASE 0x4041080
+#define CUSTOM_COUNTER_0_IRQ -1
+#define CUSTOM_COUNTER_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define CUSTOM_COUNTER_0_NAME "/dev/custom_counter_0"
+#define CUSTOM_COUNTER_0_SPAN 32
+#define CUSTOM_COUNTER_0_TYPE "custom_counter_0"
+
+
+/*
+ * custom_counter_2 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_custom_counter_2 custom_counter_0
+#define CUSTOM_COUNTER_2_BASE 0x4041040
+#define CUSTOM_COUNTER_2_IRQ -1
+#define CUSTOM_COUNTER_2_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define CUSTOM_COUNTER_2_NAME "/dev/custom_counter_2"
+#define CUSTOM_COUNTER_2_SPAN 32
+#define CUSTOM_COUNTER_2_TYPE "custom_counter_0"
 
 
 /*
@@ -224,26 +225,26 @@
 
 #define ALT_INCLUDE_INSTRUCTION_RELATED_EXCEPTION_API
 #define ALT_MAX_FD 32
-#define ALT_SYS_CLK TIMER_1
+#define ALT_SYS_CLK TIMER_0
 #define ALT_TIMESTAMP_CLK none
 
 
 /*
- * jtag_uart_1 configuration
+ * jtag_uart_0 configuration
  *
  */
 
-#define ALT_MODULE_CLASS_jtag_uart_1 altera_avalon_jtag_uart
-#define JTAG_UART_1_BASE 0x40410d0
-#define JTAG_UART_1_IRQ 2
-#define JTAG_UART_1_IRQ_INTERRUPT_CONTROLLER_ID 0
-#define JTAG_UART_1_NAME "/dev/jtag_uart_1"
-#define JTAG_UART_1_READ_DEPTH 64
-#define JTAG_UART_1_READ_THRESHOLD 8
-#define JTAG_UART_1_SPAN 8
-#define JTAG_UART_1_TYPE "altera_avalon_jtag_uart"
-#define JTAG_UART_1_WRITE_DEPTH 64
-#define JTAG_UART_1_WRITE_THRESHOLD 8
+#define ALT_MODULE_CLASS_jtag_uart_0 altera_avalon_jtag_uart
+#define JTAG_UART_0_BASE 0x40410e8
+#define JTAG_UART_0_IRQ 3
+#define JTAG_UART_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define JTAG_UART_0_NAME "/dev/jtag_uart_0"
+#define JTAG_UART_0_READ_DEPTH 64
+#define JTAG_UART_0_READ_THRESHOLD 8
+#define JTAG_UART_0_SPAN 8
+#define JTAG_UART_0_TYPE "altera_avalon_jtag_uart"
+#define JTAG_UART_0_WRITE_DEPTH 64
+#define JTAG_UART_0_WRITE_THRESHOLD 8
 
 
 /*
@@ -252,9 +253,9 @@
  */
 
 #define ALT_MODULE_CLASS_mailbox_simple_0 altera_avalon_mailbox_simple
-#define MAILBOX_SIMPLE_0_BASE 0x4041080
-#define MAILBOX_SIMPLE_0_IRQ 0
-#define MAILBOX_SIMPLE_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define MAILBOX_SIMPLE_0_BASE 0x40410b0
+#define MAILBOX_SIMPLE_0_IRQ -1
+#define MAILBOX_SIMPLE_0_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define MAILBOX_SIMPLE_0_NAME "/dev/mailbox_simple_0"
 #define MAILBOX_SIMPLE_0_SPAN 16
 #define MAILBOX_SIMPLE_0_TYPE "altera_avalon_mailbox_simple"
@@ -266,7 +267,7 @@
  */
 
 #define ALT_MODULE_CLASS_mutex_0 altera_avalon_mutex
-#define MUTEX_0_BASE 0x40410c8
+#define MUTEX_0_BASE 0x40410e0
 #define MUTEX_0_IRQ -1
 #define MUTEX_0_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define MUTEX_0_NAME "/dev/mutex_0"
@@ -279,64 +280,46 @@
 
 
 /*
- * mutex_1 configuration
+ * onchip_memory2_0 configuration
  *
  */
 
-#define ALT_MODULE_CLASS_mutex_1 altera_avalon_mutex
-#define MUTEX_1_BASE 0x40410c0
-#define MUTEX_1_IRQ -1
-#define MUTEX_1_IRQ_INTERRUPT_CONTROLLER_ID -1
-#define MUTEX_1_NAME "/dev/mutex_1"
-#define MUTEX_1_OWNER_INIT 0
-#define MUTEX_1_OWNER_WIDTH 16
-#define MUTEX_1_SPAN 8
-#define MUTEX_1_TYPE "altera_avalon_mutex"
-#define MUTEX_1_VALUE_INIT 0
-#define MUTEX_1_VALUE_WIDTH 16
+#define ALT_MODULE_CLASS_onchip_memory2_0 altera_avalon_onchip_memory2
+#define ONCHIP_MEMORY2_0_ALLOW_IN_SYSTEM_MEMORY_CONTENT_EDITOR 0
+#define ONCHIP_MEMORY2_0_ALLOW_MRAM_SIM_CONTENTS_ONLY_FILE 0
+#define ONCHIP_MEMORY2_0_BASE 0x4020000
+#define ONCHIP_MEMORY2_0_CONTENTS_INFO ""
+#define ONCHIP_MEMORY2_0_DUAL_PORT 0
+#define ONCHIP_MEMORY2_0_GUI_RAM_BLOCK_TYPE "AUTO"
+#define ONCHIP_MEMORY2_0_INIT_CONTENTS_FILE "soc_system_onchip_memory2_0"
+#define ONCHIP_MEMORY2_0_INIT_MEM_CONTENT 1
+#define ONCHIP_MEMORY2_0_INSTANCE_ID "NONE"
+#define ONCHIP_MEMORY2_0_IRQ -1
+#define ONCHIP_MEMORY2_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define ONCHIP_MEMORY2_0_NAME "/dev/onchip_memory2_0"
+#define ONCHIP_MEMORY2_0_NON_DEFAULT_INIT_FILE_ENABLED 0
+#define ONCHIP_MEMORY2_0_RAM_BLOCK_TYPE "AUTO"
+#define ONCHIP_MEMORY2_0_READ_DURING_WRITE_MODE "DONT_CARE"
+#define ONCHIP_MEMORY2_0_SINGLE_CLOCK_OP 0
+#define ONCHIP_MEMORY2_0_SIZE_MULTIPLE 1
+#define ONCHIP_MEMORY2_0_SIZE_VALUE 131072
+#define ONCHIP_MEMORY2_0_SPAN 131072
+#define ONCHIP_MEMORY2_0_TYPE "altera_avalon_onchip_memory2"
+#define ONCHIP_MEMORY2_0_WRITABLE 1
 
 
 /*
- * onchip_memory_1 configuration
+ * parallel_port_0 configuration
  *
  */
 
-#define ALT_MODULE_CLASS_onchip_memory_1 altera_avalon_onchip_memory2
-#define ONCHIP_MEMORY_1_ALLOW_IN_SYSTEM_MEMORY_CONTENT_EDITOR 0
-#define ONCHIP_MEMORY_1_ALLOW_MRAM_SIM_CONTENTS_ONLY_FILE 0
-#define ONCHIP_MEMORY_1_BASE 0x4020000
-#define ONCHIP_MEMORY_1_CONTENTS_INFO ""
-#define ONCHIP_MEMORY_1_DUAL_PORT 0
-#define ONCHIP_MEMORY_1_GUI_RAM_BLOCK_TYPE "AUTO"
-#define ONCHIP_MEMORY_1_INIT_CONTENTS_FILE "soc_system_onchip_memory_1"
-#define ONCHIP_MEMORY_1_INIT_MEM_CONTENT 1
-#define ONCHIP_MEMORY_1_INSTANCE_ID "NONE"
-#define ONCHIP_MEMORY_1_IRQ -1
-#define ONCHIP_MEMORY_1_IRQ_INTERRUPT_CONTROLLER_ID -1
-#define ONCHIP_MEMORY_1_NAME "/dev/onchip_memory_1"
-#define ONCHIP_MEMORY_1_NON_DEFAULT_INIT_FILE_ENABLED 0
-#define ONCHIP_MEMORY_1_RAM_BLOCK_TYPE "AUTO"
-#define ONCHIP_MEMORY_1_READ_DURING_WRITE_MODE "DONT_CARE"
-#define ONCHIP_MEMORY_1_SINGLE_CLOCK_OP 0
-#define ONCHIP_MEMORY_1_SIZE_MULTIPLE 1
-#define ONCHIP_MEMORY_1_SIZE_VALUE 131072
-#define ONCHIP_MEMORY_1_SPAN 131072
-#define ONCHIP_MEMORY_1_TYPE "altera_avalon_onchip_memory2"
-#define ONCHIP_MEMORY_1_WRITABLE 1
-
-
-/*
- * parallel_port_1 configuration
- *
- */
-
-#define ALT_MODULE_CLASS_parallel_port_1 altera_up_avalon_parallel_port
-#define PARALLEL_PORT_1_BASE 0x40410b0
-#define PARALLEL_PORT_1_IRQ -1
-#define PARALLEL_PORT_1_IRQ_INTERRUPT_CONTROLLER_ID -1
-#define PARALLEL_PORT_1_NAME "/dev/parallel_port_1"
-#define PARALLEL_PORT_1_SPAN 16
-#define PARALLEL_PORT_1_TYPE "altera_up_avalon_parallel_port"
+#define ALT_MODULE_CLASS_parallel_port_0 altera_up_avalon_parallel_port
+#define PARALLEL_PORT_0_BASE 0x40410d0
+#define PARALLEL_PORT_0_IRQ -1
+#define PARALLEL_PORT_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define PARALLEL_PORT_0_NAME "/dev/parallel_port_0"
+#define PARALLEL_PORT_0_SPAN 16
+#define PARALLEL_PORT_0_TYPE "altera_up_avalon_parallel_port"
 
 
 /*
@@ -345,7 +328,7 @@
  */
 
 #define ALT_MODULE_CLASS_parallel_port_2 altera_up_avalon_parallel_port
-#define PARALLEL_PORT_2_BASE 0x40410a0
+#define PARALLEL_PORT_2_BASE 0x40410c0
 #define PARALLEL_PORT_2_IRQ -1
 #define PARALLEL_PORT_2_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define PARALLEL_PORT_2_NAME "/dev/parallel_port_2"
@@ -354,18 +337,45 @@
 
 
 /*
- * performance_counter_1 configuration
+ * performance_counter_0 configuration
  *
  */
 
-#define ALT_MODULE_CLASS_performance_counter_1 altera_avalon_performance_counter
-#define PERFORMANCE_COUNTER_1_BASE 0x4041000
-#define PERFORMANCE_COUNTER_1_HOW_MANY_SECTIONS 3
-#define PERFORMANCE_COUNTER_1_IRQ -1
-#define PERFORMANCE_COUNTER_1_IRQ_INTERRUPT_CONTROLLER_ID -1
-#define PERFORMANCE_COUNTER_1_NAME "/dev/performance_counter_1"
-#define PERFORMANCE_COUNTER_1_SPAN 64
-#define PERFORMANCE_COUNTER_1_TYPE "altera_avalon_performance_counter"
+#define ALT_MODULE_CLASS_performance_counter_0 altera_avalon_performance_counter
+#define PERFORMANCE_COUNTER_0_BASE 0x4041000
+#define PERFORMANCE_COUNTER_0_HOW_MANY_SECTIONS 3
+#define PERFORMANCE_COUNTER_0_IRQ -1
+#define PERFORMANCE_COUNTER_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define PERFORMANCE_COUNTER_0_NAME "/dev/performance_counter_0"
+#define PERFORMANCE_COUNTER_0_SPAN 64
+#define PERFORMANCE_COUNTER_0_TYPE "altera_avalon_performance_counter"
+
+
+/*
+ * pio_2 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_pio_2 altera_avalon_pio
+#define PIO_2_BASE 0x40410a0
+#define PIO_2_BIT_CLEARING_EDGE_REGISTER 1
+#define PIO_2_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define PIO_2_CAPTURE 1
+#define PIO_2_DATA_WIDTH 8
+#define PIO_2_DO_TEST_BENCH_WIRING 0
+#define PIO_2_DRIVEN_SIM_VALUE 0
+#define PIO_2_EDGE_TYPE "RISING"
+#define PIO_2_FREQ 50000000
+#define PIO_2_HAS_IN 1
+#define PIO_2_HAS_OUT 0
+#define PIO_2_HAS_TRI 0
+#define PIO_2_IRQ 4
+#define PIO_2_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define PIO_2_IRQ_TYPE "EDGE"
+#define PIO_2_NAME "/dev/pio_2"
+#define PIO_2_RESET_VALUE 0
+#define PIO_2_SPAN 16
+#define PIO_2_TYPE "altera_avalon_pio"
 
 
 /*
@@ -408,28 +418,28 @@
 
 
 /*
- * timer_1 configuration
+ * timer_0 configuration
  *
  */
 
-#define ALT_MODULE_CLASS_timer_1 altera_avalon_timer
-#define TIMER_1_ALWAYS_RUN 0
-#define TIMER_1_BASE 0x4041040
-#define TIMER_1_COUNTER_SIZE 32
-#define TIMER_1_FIXED_PERIOD 0
-#define TIMER_1_FREQ 50000000
-#define TIMER_1_IRQ 3
-#define TIMER_1_IRQ_INTERRUPT_CONTROLLER_ID 0
-#define TIMER_1_LOAD_VALUE 49999
-#define TIMER_1_MULT 0.001
-#define TIMER_1_NAME "/dev/timer_1"
-#define TIMER_1_PERIOD 1
-#define TIMER_1_PERIOD_UNITS "ms"
-#define TIMER_1_RESET_OUTPUT 0
-#define TIMER_1_SNAPSHOT 1
-#define TIMER_1_SPAN 32
-#define TIMER_1_TICKS_PER_SEC 1000
-#define TIMER_1_TIMEOUT_PULSE_OUTPUT 0
-#define TIMER_1_TYPE "altera_avalon_timer"
+#define ALT_MODULE_CLASS_timer_0 altera_avalon_timer
+#define TIMER_0_ALWAYS_RUN 0
+#define TIMER_0_BASE 0x4041060
+#define TIMER_0_COUNTER_SIZE 32
+#define TIMER_0_FIXED_PERIOD 0
+#define TIMER_0_FREQ 50000000
+#define TIMER_0_IRQ 2
+#define TIMER_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define TIMER_0_LOAD_VALUE 49999
+#define TIMER_0_MULT 0.001
+#define TIMER_0_NAME "/dev/timer_0"
+#define TIMER_0_PERIOD 1
+#define TIMER_0_PERIOD_UNITS "ms"
+#define TIMER_0_RESET_OUTPUT 0
+#define TIMER_0_SNAPSHOT 1
+#define TIMER_0_SPAN 32
+#define TIMER_0_TICKS_PER_SEC 1000
+#define TIMER_0_TIMEOUT_PULSE_OUTPUT 0
+#define TIMER_0_TYPE "altera_avalon_timer"
 
 #endif /* __SYSTEM_H_ */
