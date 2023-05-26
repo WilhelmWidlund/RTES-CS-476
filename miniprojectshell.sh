2a4345eeb59690eac68b1d2f0aa503de27748a9c
Elias De Smijter <elias.desmijter@student.kuleuven.be>
2023-05-26 19:04:24 +0200

    shell script test

--- miniprojectshell.sh
+++ miniprojectshell.sh
@@ -3,5 +3,9 @@ echo 'Starting programs on both processors...'
 
 cd miniproject_2/sw/nios/application_0
 nios2-download -g cpu_0_proj.elf --device 2 --instance 0
+echo 'Process on cpu 0 (audio) started'
 cd ../application_1
 nios2-download -g cpu_1_proj.elf --device 2 --instance 1
+echo 'Process on cpu 1 (SigProc) started'
+
+gnome-terminal -e echo 'TEST'
